9849b1794b6929b8735975513cf63a80a681074b
dembasy <dembasy1227@gmail.com>
2018-07-16 09:51:42 +0200

    recuperation

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var myShop = angular.module('shop', []);
+
+myShop.run(function ($rootScope) {
+  $rootScope.quantity = {};
+  $rootScope.items = [];
+});
+
+myShop.controller('myContent', ['$scope', '$http', function ($scope, $http) {
+
+}]);
+myShop.controller('myCart', ['$scope', function($scope){
+
+}]);
