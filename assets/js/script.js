b61d65769bea349fa0d6018a58317a3471da2e87
Stephan <stephannolas96@hotmail.com>
2018-07-16 11:15:32 +0200

    modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var myShop = angular.module('shop', []);
 
-myShop.run(function ($rootScope) {
-  $rootScope.quantity = {};
-  $rootScope.items = [];
+myShop.run(function ($rootScope, $http) {
+  $rootScope.qantity = {};
+  $http.get('assets/database.json').then(function (response){
+    $rootScope.items = response.data;
+  });
+
 });
 
-myShop.controller('myContent', ['$scope', '$http', function ($scope, $http) {
-
+myShop.controller('myContent', ['$scope', function ($scope) {
+  $scope.addToBasket = function(index) {
+    console.log($scope.qantity);
+    if(!(index in $scope.qantity)){
+      $scope.qantity[index] = 1;
+    } else {
+      $scope.qantity[index]++;
+    }
+  };
 }]);
 
 myShop.controller('myCart', ['$scope', function($scope){
