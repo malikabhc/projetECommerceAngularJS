66a02551fef9eb76376e2ba99a5a070d5c322301
dembasy <dembasy1227@gmail.com>
2018-07-17 08:47:52 +0200

    recup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
+// Définition du Module Angularjs
 var myShop = angular.module('shop', []);
-
+// Definition du run qui s'executera une fois au demarrage de la page
 myShop.run(function ($rootScope, $http) {
+// Definition des rootScope qui seront utilisées dans les controller
   $rootScope.qantities = {};
   $rootScope.total = 0;
   $rootScope.totalQantity = 0;
   $rootScope.itemsCategories = '';
+// Lien vers le base json
   $http.get('assets/database.json').then(function (response) {
     $rootScope.items = response.data;
   });
 
 });
-
+// Definition du controller qui va contenir les différentes fonctions du panier et des éléments
 myShop.controller('myContent', ['$scope', function ($scope) {
+  // fonction qui va nous permettre d'ajouter des produits au panier
   $scope.addToBasket = function (index) {
     if(!(index in $scope.qantities)) {
       $scope.qantities[index] = 1;
@@ -21,11 +25,13 @@ myShop.controller('myContent', ['$scope', function ($scope) {
     $scope.totalCalc();
     $scope.totalQantityCalc();
   };
+  // Definition de la fonction qui va permettre de supprimer des élements dans le panier
   $scope.erase = function (index) {
     delete $scope.qantities[index];
     $scope.totalCalc();
     $scope.totalQantityCalc();
   };
+  // defintion de la fonction qui va permettre de changer de quantité dans le panier
   $scope.qantityChange = function(index, val) {
     if($scope.qantities[index] + val <= 0)
     {
@@ -35,6 +41,7 @@ myShop.controller('myContent', ['$scope', function ($scope) {
     $scope.totalCalc();
     $scope.totalQantityCalc();
   };
+  // boucle et fonction permettant de faire le calcule du total 
   $scope.totalCalc = function(){
     $scope.total = 0;
     for (var key in $scope.qantities) {
@@ -47,8 +54,7 @@ myShop.controller('myContent', ['$scope', function ($scope) {
     $scope.totalQantity += $scope.qantities[key];
     }
   };
-  $scope.changeFilter = function(newFilter, $event){
+  $scope.changeFilter = function(newFilter){
       $scope.itemsCategories = newFilter;
-      console.log($event.target);
   }
 }]);
